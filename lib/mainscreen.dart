9a246708d7dd8ea2ae473527f3b97faa18e987ab
Perumallpardhiv <LIT2021001@iiitl.ac.in>
2023-02-12 12:27:03 +0530

    Started AI project

---
+++ lib/mainscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class mainScreen extends StatefulWidget {
+  const mainScreen({super.key});
+
+  @override
+  State<mainScreen> createState() => _mainScreenState();
+}
+
+class _mainScreenState extends State<mainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
